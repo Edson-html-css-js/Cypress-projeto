99638759b5547456d0098e8eebe3d4cdf2cba143
Edson Lucas <ed_ls@outlook.com>
2022-03-23 11:05:49 -0300

    Cadastro no site, com caminho não feliz

---
+++ cypress/integration/3-alurapic/alurapi.spec.js
@@ -0,0 +1,25 @@
+/// <reference types="Cypress" />
+
+describe('login com sussesso', () => {
+
+
+    beforeEach(() => {
+        cy.visit('https://alura-fotos.herokuapp.com')
+
+    })
+
+
+
+    it('Verifica mensagens de validacao', () => {
+
+        cy.contains('a','Register now').click()
+        cy.contains('button','Register').click()
+        cy.contains('ap-vmessage','Email is required!').should('be.visible')
+        cy.contains('button','Register').click()
+        cy.contains('ap-vmessage','Full name is required!').should('be.visible')
+        cy.contains('ap-vmessage','User name is required!').should('be.visible')
+        cy.contains('ap-vmessage','Password is required!').should('be.visible')
+
+})
+
+})
